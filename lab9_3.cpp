f5b892718315e037458671b83110b2d199b45c2f
earthx15 <uncles1512@hotmail.com>
2021-01-08 16:19:27 +0700

    pass

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int n=0;float sum=0,sd=0;
+    ifstream source;
+    string textline;
+    source.open("score.txt");
+    while (getline(source,textline))
+	{
+        n++;
+        sum+=atof(textline.c_str());
+        
+	}
+    source.close();
+    cout << "Number of data = "<<n;
+    cout << setprecision(3);
+    sum=sum/n;
+    cout << "\nMean = "<<sum;
+    source.open("score.txt");
+    while (getline(source,textline))
+	{
+        sd=sd+pow(atof(textline.c_str())-sum,2);
+	}
+    source.close();
+    cout << "\nStandard deviation = "<< sqrt(sd/n);
+}
